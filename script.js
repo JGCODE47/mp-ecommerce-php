210364a3a6e3ed13135ad9d79348b358da19937f
Jhonatan Jair Garcia Alfaro <joga47uk@gmail.com>
2023-05-30 11:55:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const mp = new MercadoPago('TEST-6d829d75-1d24-4339-a5b8-61fc5824d9d5');
 const bricksBuilder = mp.bricks();
 
 
-mp.bricks().create("wallet", "wallet_container", {
+mp.bricks().create("wallet", "pagarProducto", {
     initialization: {
-        preferenceId: "<pagarProducto>",
+        preferenceId: "<>",
     },
  });
