09a4da092a7f6dbb17b34fcdcd8eb7fcadd15f34
Jhonatan Jair Garcia Alfaro <joga47uk@gmail.com>
2023-05-31 15:28:05 -0500

    Update server.php

--- server/server.php
+++ server/server.php
@@ -5,7 +5,7 @@ require __DIR__  . '/vendor/autoload.php';
 //REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
 MercadoPago\SDK::setAccessToken("APP_USR-6077109975925282-092815-2835b9088a8e0364c5ebef48ec8743f4-195885622");
 
-if (isset($_POST)) {
+
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();
     // Crea un ítem en la preferencia
@@ -28,4 +28,4 @@ if (isset($_POST)) {
         'id' => $preference->id,
     ); 
     echo json_encode($response);
-}
+
