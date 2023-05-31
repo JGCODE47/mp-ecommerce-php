8eacdf4dbbe32d0e423241b964b776b76cbaeb9c
Jhonatan Jair Garcia Alfaro <joga47uk@gmail.com>
2023-05-31 15:40:22 -0500

    up

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const btn = document.querySelector(".mercadopago-button")
 
-btn.addEventListener("click", (e)=>{
+btn.addEventListener("click", (e) => {
     const date = new FormData();
     date.append("name_product", e.target.getAttribute("name_product"))
     date.append("price", parseInt(e.target.getAttribute("price")))
     date.append("unit", parseInt(e.target.getAttribute("unit")))
     let opt = {
-        method:"POST",
-        body:date
+        method: "POST",
+        body: date
     }
-    async function Preferenc(){
+    async function Preferenc() {
         const response = await fetch("https://jgcode47-mp-ecommerce-php.herokuapp.com/server/server.php", opt)
     }
-    Promise.resolve(Preferenc()).then(item=>{
+    Promise.resolve(Preferenc()).then(item => {
+        const mp = new MercadoPago('APP_USR-f67862ea-f70e-4ae4-8e99-80dcff04b630');
+        const bricksBuilder = mp.bricks();
         mp.bricks().create("wallet", "mercadopago", {
             initialization: {
                 preferenceId: `${item.id}`,
             },
-         });
+        });
     })
 })
 
@@ -26,4 +28,3 @@ btn.addEventListener("click", (e)=>{
 
 
 
- 
